7b8f9e6a4d2b3900c3456843f816ae918bb6c9a1
Jay <jaykidokenya@gmail.com>
2023-12-03 03:50:33 -0800

    typo fixing

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,92 +1,98 @@
-"use client"
+"use client";
 
-import * as z from 'zod'
-import {zodResolver} from '@hookform/resolvers/zod'
-import axios from 'axios'
-import { useForm } from 'react-hook-form'
-import { useRouter } from 'next/navigation'
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from "@/components/ui/form"
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import Link from 'next/link'
-import toast from 'react-hot-toast'
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
-    title: z.string().min(1, {message: "title is Required"})
-})
+  title: z.string().min(1, { message: "title is Required" }),
+});
 
 const Createpage = () => {
-    const router = useRouter()
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            title: ""
-        },
-    })
-    const {isSubmitting, isValid} = form.formState
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  const { isSubmitting, isValid } = form.formState;
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-       try {
-        console.log("I am here", values)
-        const response = await axios.post("/api/courses", values)
-        router.push(`/teacher/courses/${response.data.id}`)
-        toast.success("Course created 🎉")
-       } catch {
-       toast.error("Something went wrong")
-       }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log("I am here", values);
+      const response = await axios.post("/api/courses", values);
+      console.log("The response is", response);
+      router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course created 🎉");
+    } catch {
+      toast.error("Something went wrong");
     }
+  };
 
-    return ( 
-        <div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6'>
-            <div>
-                <h1 className='text-2xl'>
-                    Name Your Course
-                </h1>
-                <p className='text-sm text-slate-600'>What name suits your course? 🤔</p> 
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)}        className='space-y-8 mt-8'>
-                        <FormField
-                        control={form.control}
-                        name='title'
-                        render={({ field }) => (
-                            <FormItem>
-                            <FormLabel>Course Title</FormLabel>
-                            <FormControl>
-                                <Input
-                                disabled={isSubmitting}
-                                placeholder='e.g information Security'
-                                {...field}
-                                />
-                            </FormControl>
-                            <FormDescription>What will be You be teaching us? 😁</FormDescription>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                        />
-                        <div className='flex items-center gap-x-2'>
-                        <Link href='/'>
-                            <Button type='button' variant='ghost'>
-                            Cancel
-                            </Button>
-                        </Link>
-                        <Button type='submit' disabled={!isValid || isSubmitting}>
-                            Continue
-                        </Button>
-                        </div>
-                    </form>
-                </Form>
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">Name Your Course</h1>
+        <p className="text-sm text-slate-600">
+          What name suits your course? 🤔
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g information Security"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will be You be teaching us?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
             </div>
-        </div>
-     );
-}
- 
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
 export default Createpage;
