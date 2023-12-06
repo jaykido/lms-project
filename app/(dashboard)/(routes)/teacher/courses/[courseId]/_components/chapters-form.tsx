2871fcb45b82ac3235eae72b49540c3364cd716d
Jay <jaykidokenya@gmail.com>
2023-12-06 17:02:55 +0300

    Course Chapter Updating API + UI

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -13,7 +13,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -21,6 +21,7 @@ import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Chapter, Course } from "@prisma/client";
+import { ChapterList } from "./chapters-list";
 
 interface ChapterFormProps {
   initialData: Course & { chapters: Chapter[] };
@@ -60,8 +61,33 @@ export const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
     }
   };
 
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast.success("Re-Arranged");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong😑");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/course/${courseId}/chapter/${id}`);
+  };
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -112,7 +138,11 @@ export const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
           )}
         >
           {!initialData.chapters.length && "No chapters"}
-          {/* Add a list of chapters */}
+          <ChapterList
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
         </div>
       )}
       {!isCreating && (
---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapter/[chapterId]/page.tsx
@@ -0,0 +1,5 @@
+const ChapterIdPage = () => {
+  return <div>Chapter ID</div>;
+};
+
+export default ChapterIdPage;
