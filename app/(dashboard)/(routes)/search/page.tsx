b7b5c8ff93b1d464d499881646f6568e2dcb8281
Jay <jaykidokenya@gmail.com>
2023-12-15 09:49:57 +0300

    Displaying the videos

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,11 +2,13 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-// import { SearchInput } from "@/components/search-input";
-// import { getCourses } from "@/actions/get-courses";
+
 // import { CoursesList } from "@/components/courses-list";
 
 import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
 
 interface SearchPageProps {
   searchParams: {
@@ -28,21 +30,22 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
     },
   });
 
-    // const courses = await getCourses({
-    //   userId,
-    //   ...searchParams,
-    // });
+    const courses = await getCourses({
+      userId,
+      ...searchParams,
+    });
 
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
-        {/* <SearchInput /> */}
+        <SearchInput />
       </div>
       <div className="p-6 space-y-4">
         <Categories
           items={categories}
         />
-        {/* <CoursesList items={courses} /> */}
+        <CoursesList items={courses} />
+
       </div>
     </>
   );
